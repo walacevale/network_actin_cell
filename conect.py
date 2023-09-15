21dbc342b35d57a8f1f1d292da14c30dc6be7741
Wallace Vale <wallacevale123@gmail.com>
2023-09-15 09:34:16 -0300

    feat: img novas t0 a t30

--- conect.py
+++ conect.py
@@ -6,20 +6,24 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from skimage.morphology import skeletonize
 
-arr = gray(cv2.imread('1.png'))
-
-plt.imshow(arr)
-plt.show()
+img_th = th_otsu(gray(cv2.imread('1.png')))
 
 
-sk = skeletonize(arr,  method= 'zhang')
-plt.imshow(sk)
-plt.show()
+arr = skeletonize(img_th,  method= 'zhang')
 
 def get_neighbors(i, j, arr):
     rows, cols = arr.shape
-    offsets = [(-1, 0), (0, -1), (0, 1), (1, 0)]
-    neighbors = [(i + offset[0], j + offset[1]) for offset in offsets if 0 <= i + offset[0] < rows and 0 <= j + offset[1] < cols]
+
+    # Vizinhos imediatos
+    offsets_1 = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+    # Segundos vizinhos
+    offsets_2 = [(-2, 0), (0, -2), (0, 2), (2, 0), (-2, -2), (-2, 2), (2, -2), (2, 2)]
+    
+    # Juntando todos os offsets
+    all_offsets = offsets_1 + offsets_2
+
+    neighbors = [(i + offset[0], j + offset[1]) for offset in all_offsets if 0 <= i + offset[0] < rows and 0 <= j + offset[1] < cols]
+    
     return neighbors
 
 def get_pixel_label(i, j, width):
@@ -45,7 +49,39 @@ G = nx.Graph(df)
 # Use the image coordinates for the position
 pos = {i: (i % width, arr.shape[0] - i // width - 1) for i in G.nodes()}
 
-# Draw the graph
-nx.draw(G, pos, with_labels=True)
-plt.gca().invert_yaxis()  # Invert y-axis to match the numpy array's coordinate system
+# Node specifications
+node_size = 6
+node_color = 'cyan'
+node_edge_color = 'black'
+node_alpha = 0.7
+
+# Edge specifications
+edge_width = 1.5
+edge_alpha = 0.7
+edge_color = 'gray'
+
+# Node label specifications
+label_font_size = 10
+label_font_color = 'black'
+
+# Draw the graph with the above specifications
+nx.draw(G, pos,
+        node_size=node_size, 
+        node_color=node_color, 
+        alpha=node_alpha, 
+        edge_color=edge_color, 
+        width=edge_width, 
+        with_labels=False, 
+        font_size=label_font_size,
+        node_shape='o',
+        edgecolors=node_edge_color)
+
+# Invert y-axis to match the numpy array's coordinate system
+#plt.gca().invert_yaxis()  
+
+# Set a title (optional)
+plt.title('Visualized Graph from Image')
+
+# Set the graph background color
+plt.gca().set_facecolor('lightgray')
 plt.show()
